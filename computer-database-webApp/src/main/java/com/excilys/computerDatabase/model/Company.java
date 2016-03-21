b3eedb6bad714790115dc2e86ee48a16fd7157cc
drancurello <drancurello@gmail.com>
2016-03-21 17:42:38 +0100

    ajout des methodes hashcode et equals

--- computer-database-webApp/src/main/java/com/excilys/computerDatabase/model/Company.java
+++ computer-database-webApp/src/main/java/com/excilys/computerDatabase/model/Company.java
@@ -72,6 +72,30 @@ public class Company {
 		this.name = name;
 	}
 
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Company other = (Company) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() {
 		return "id=" + id + ", name=" + name;
